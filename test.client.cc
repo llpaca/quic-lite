030a1a8d53e907a8a4ef35063952021bc8a6f9a4
FreezB11 <yashrajrao4@gmail.com>
2026-05-26 01:31:57 +0530

    tests

--- test.client.cc
+++ test.client.cc
@@ -5,22 +5,23 @@
 
 int main() {
     Beaver client;
+    if (client.establish(8080, "127.0.0.1") < 0) {std::cerr << "[client] failed to set peer\n";return 1;}
 
-    if (client.establish(8080, "127.0.0.1") < 0) {
-        std::cerr << "[client] failed to set peer\n";
-        return 1;
+    constexpr size_t PACKET_COUNT = 1900;
+    constexpr size_t PACKET_SIZE  = 5;
+
+    char msg[PACKET_COUNT * PACKET_SIZE] = {};
+
+    // fill stream
+    for (size_t i = 0; i < PACKET_COUNT; i++) {
+        msg[i] = static_cast<char>(i);
     }
 
-    const char *msg = "hello from beaver client";
     int sent = client.send_stream(
-        reinterpret_cast<const byte *>(msg),
-        strlen(msg)
+        reinterpret_cast<const byte*>(msg),
+        sizeof(msg)
     );
 
-    if (sent < 0) {
-        std::cerr << "[client] send_stream failed\n";
-        return 1;
-    }
-
+    if (sent < 0) {std::cerr << "[client] send_stream failed\n";return 1;}
     return 0;
 }
--- test.server.cc
+++ test.server.cc
@@ -5,7 +5,7 @@
 int main(){
     Beaver server; // default it is on ipv4
     server.listen_on(8080);
-    byte buf[4096];
+    byte buf[4*4096];
     // server.recv_stream()
     int received = server.recv_stream(buf, sizeof(buf));
     buf[received] = '\0';
